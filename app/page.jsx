30669e40814d555bb7ddb0defe416c34475ca617
Shahdin Salman <125382694+shahdinsalman23@users.noreply.github.com>
2024-11-11 18:43:31 +0500

    updated

--- app/page.jsx
+++ app/page.jsx
@@ -7,9 +7,9 @@ const page = () => {
   const [title, setTitle] = useState("")
   const [desc, setDesc] = useState("")
   const [mainTask, setMainTask] = useState([])
-  const submitHandler = (e) =>{
+  const submitHandler = (e) => {
     e.preventDefault()
-    setMainTask([...mainTask, {title, desc}]);
+    setMainTask([...mainTask, { title, desc }]);
     setTitle("")
     setDesc("")
     console.log(mainTask)
@@ -17,48 +17,49 @@ const page = () => {
 
   const deleteHandler = (i) => {
     let copytask = [...mainTask]
-    copytask.splice(i,1)
+    copytask.splice(i, 1)
     setMainTask(copytask)
   }
 
   let renderTask = <h2 className='text-white text-md'>No Task Available</h2>
 
-  if(mainTask.length>0){
-    renderTask = mainTask.map((t, i)=>{
+  if (mainTask.length > 0) {
+    renderTask = mainTask.map((t, i) => {
       return (
         <li key={i} className='flex items-center justify-between'>
           <div div className='py-4 px-6'>
             <h3 className='text-2xl text-white font-semibold py-1'>{t.title}</h3>
             <p className='text-xl text-white'>{t.desc}</p>
           </div>
-          <button className='text-white bg-red-700 py-2 px-6 rounded font-bold border-2 border-red-700 hover:bg-transparent' onClick={()=>{deleteHandler(i)}}>Delete</button>
+          <button className='text-white bg-red-700 py-2 px-6 rounded font-bold border-2 border-red-700 hover:bg-transparent' onClick={() => { deleteHandler(i) }}>Delete</button>
         </li>
       );
     })
   }
-  
+
 
   return (
     <>
 
-
+    <div className='flex items-center justify-center bg-zinc-800 mt-10 mb-5 w-[50%] m-auto'>
       <form onSubmit={submitHandler}>
-        <input type="text" name="" id="" className='text-2xl border-zinc-800 border-2 m-8 px-4 py-2' 
-        placeholder='Enter Title here'
-        value={title} 
-        onChange={(e)=>{ setTitle(e.target.value) }} 
-       />
-        <input type="text" name="" id="" className='text-2xl border-zinc-800 border-2 m-8 px-4 py-2'
-        placeholder='Enter Description here'
-        value={desc}
-        onChange={(e)=>{ setDesc(e.target.value) }} 
+        <input type="text" name="" id="" className='text-xl text-white border-zinc-800 border-2 m-8 px-4 py-2 bg-zinc-700 rounded-lg'
+          placeholder='Enter Title here'
+          value={title}
+          onChange={(e) => { setTitle(e.target.value) }}
         />
-        <button className='bg-black text-white px-6 m-5 py-3 text-xl rounded'>Add Task</button>
+        <input type="text" name="" id="" className='text-xl text-white border-zinc-800 border-2 my-8 px-4 py-2 bg-zinc-700 rounded-lg'
+          placeholder='Enter Description here'
+          value={desc}
+          onChange={(e) => { setDesc(e.target.value) }}
+        />
+        <button className='bg-black text-white px-6 m-5 py-3 text-xl rounded-lg'>Add Task</button>
       </form>
+      </div>
 
-      
-      <div className="bg-zinc-700 py-4 px-6 mx-10 w-2/3">
-        <ul>
+
+      <div className="bg-zinc-800 mx-auto mt-10 flex items-center justify-center w-[50%] py-4 px-6">
+        <ul className='w-full'>
           {renderTask}
         </ul>
       </div>
